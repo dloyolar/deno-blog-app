a55b3ddefbd96a0a36e09c7a2d5bc4f7013c2f1d
dloyolar <dieegolr@gmail.com>
2023-02-22 10:52:05 -0300

    ✨ feat(blog): add dynamic data from props in post

--- routes/blog/[id].tsx
+++ routes/blog/[id].tsx
@@ -1,9 +1,28 @@
-const PagePost = () => {
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { loadPost } from "../../utils/posts.ts";
+import { CSS } from "https://deno.land/x/gfm@0.1.26/mod.ts";
+
+export const handler: Handlers = {
+  async GET(request, context) {
+    const { id } = context.params;
+    const post = await loadPost(id);
+    return context.render({ post });
+  },
+};
+
+const PagePost = (props: PageProps) => {
+  const { post } = props?.data || {};
   return (
     <article class="p-4">
       <h1 class="text-2xl font-bold">
-        Esto es el articulo
+        {post.title}
       </h1>
+      <time>{Intl.DateTimeFormat("es").format(post.date)}</time>
+      <style dangerouslySetInnerHTML={{ __html: CSS }} />
+      <div
+        class="markdown-body"
+        dangerouslySetInnerHTML={{ __html: post.body }}
+      />
     </article>
   );
 };
